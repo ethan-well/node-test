7b2d69816f0432aecfcc9d5adad9752b6e951cda
魏巍 <wewin11235@163.com>
2018-11-13 23:45:35 +0800

    feat(learn-node): add user models

---
+++ express2/shoutbox/middleware/user.js
@@ -0,0 +1,11 @@
+const user = require('../models/user');
+
+module.exports = (req, res, next) => {
+  const uid = req.session.uid;
+  if (!uid) return next();
+  User.get(uid, (err, user) {
+    if (err) return next(err);
+    req.user = res.locals.user = user;
+    next();
+  });
+};
