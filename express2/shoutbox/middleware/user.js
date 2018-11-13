62746e20d65733f0970e3fc24a6cc587a3f3625b
魏巍 <wewin11235@163.com>
2018-11-13 23:52:50 +0800

    feat(learn-node): fix error of user middleware

--- express2/shoutbox/middleware/user.js
+++ express2/shoutbox/middleware/user.js
@@ -3,7 +3,7 @@ const user = require('../models/user');
 module.exports = (req, res, next) => {
   const uid = req.session.uid;
   if (!uid) return next();
-  User.get(uid, (err, user) {
+  User.get(uid, (err, user) => {
     if (err) return next(err);
     req.user = res.locals.user = user;
     next();
