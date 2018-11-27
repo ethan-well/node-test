ad9e8c5abd8a65435595c9718419edbcbbee097d
魏巍 <wewin11235@163.com>
2018-11-27 13:09:17 +0800

    fix(learn-node): fix some error

--- testReqBody/server.js
+++ testReqBody/server.js
@@ -6,9 +6,8 @@ const server = http.createServer((req, res) => {
     req.on('data', chunk => {
       data += chunk.toString();
       console.log('data', chunk.toString());
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end('Hi, ' + data);
     });
-
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end('Hi, ' + data);
   }
 }).listen(3300);
