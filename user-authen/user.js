f7b8186cb4d3481d191a2b8240f3db5aff40bfc9
魏巍 <wewin11235@163.com>
2018-11-29 23:11:00 +0800

    feat(learn-node): add user model

---
+++ user-authen/user.js
@@ -0,0 +1,13 @@
+const redis = require('redis');
+const bcrypt = require('bcrypt');
+const db = redis.createClient();
+
+class User {
+  constructor(obj) {
+    for (let key in obj) {
+      this[key] = obj[key];
+    }
+  }
+}
+
+module.exports = User;
