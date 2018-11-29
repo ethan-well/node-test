472962983598513bc1592f349055e29c93445ea7
魏巍 <wewin11235@163.com>
2018-11-29 23:22:31 +0800

    feat(learn-node): finished user modal

--- user-authen/user.js
+++ user-authen/user.js
@@ -8,6 +8,43 @@ class User {
       this[key] = obj[key];
     }
   }
+
+  save(cb) {
+    if (this.id) {
+      this.update(cb);
+    } else {
+      db.incr('user:ids', (err, id) => {
+        if (err) return cb(err);
+        this.id = id;
+        this.hashPassword((err) => {
+          if (err) return cb(err);
+          this.update(cb);
+        });
+      });
+    }
+  }
+
+  update(cb) {
+    const id = this.id;
+    db.set(`user:id${this.name}`, id, (err) => {
+      if (err) return cb(err);
+      db.hmset(`user:${id}`, this, (err) => {
+        cb(err);
+      });
+    });
+  }
+
+  hashPassword(cb) {
+    bcrypt.genSalt(12, (err, aalt) => {
+      if (err) return cb(err);
+      this.salt = salt;
+      bcrypt.hash(this.hashPassword, salt, (err, hash) => {
+        if (err) return cb(err);
+        this.pass = hash;
+        cb();
+      });
+    });
+  }
 }
 
 module.exports = User;
